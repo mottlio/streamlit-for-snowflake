87cd2b5c42b5c3267d7183876862c8ba7dd354bf
michalm <michal.mottl@comcom.govt.nz>
2024-07-01 15:35:36 +1200

    feat: Add Snowflake query and display results in Streamlit app

--- 3-connect-to-snowflake/08-streamlit-connector/Main.py
+++ 3-connect-to-snowflake/08-streamlit-connector/Main.py
@@ -25,3 +25,9 @@ data = pd.DataFrame({
 if submit_button:
     st.dataframe(data)
 
+conn = st.connection("snowflake")
+df = conn.query("select * from DATAANALYTICS_DB_DEV.DBT_MICHALM.calculate_icf_flows_2023_approach3",
+    ttl=3600, show_spinner="Running query...")
+
+st.dataframe(df)
+
